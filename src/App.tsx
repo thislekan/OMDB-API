f8ed8045731cd0bc55d0a64401b92fa37679651c
Lekan Omoniyi <ibrahimlekanomoniyi@gmail.com>
2022-12-19 04:10:24 +0100

    - modified existing files

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { SearchComponent } from "./components/search";
+import { TileView } from "./components/view";
+import { MovieContextProvider } from "./context/movieContext";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MovieContextProvider>
+      <div className="App">
+        <div className="App__wrapper">
+          <div className="app-title">
+            <h1>Movie Finder</h1>
+            <p>The best app to find that movie</p>
+          </div>
+          <SearchComponent />
+          <TileView />
+        </div>
+      </div>
+    </MovieContextProvider>
   );
 }
 
